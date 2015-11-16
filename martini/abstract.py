8f9f8c43d49f80d4373d1e363cd19e2146a6d6a6
Peter <Pitje06@users.noreply.github.com>
2015-11-16 14:45:58 +0100

    Corrected an import typo, added some more abstract classes and methods

--- martini/abstract.py
+++ martini/abstract.py
@@ -2,8 +2,7 @@
 """
 Provides abstract base classes for future use.
 """
-from compat import *
-
+from .compat import *
 import abc
 
 
@@ -17,9 +16,64 @@ class ABC(with_metaclass(abc.ABCMeta, object)):
                    for attr in cls.__abstractmethods__)
 
 
-# You do NOT need to inherit from this class to be it's subclass.
+# You do NOT need to inherit from these classes to be it's subclass.
 # All you need to do is implement all it's abstract methods.
 class Structure(ABC):
     @abc.abstractproperty
     def atoms(self):
         return None
+
+    @abc.abstractproperty
+    def residues(self):
+        return []
+
+
+class Trajectory(ABC):
+    @abc.abstractmethod
+    def __iter__(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def __len__(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def __getitem__(self, key):
+        raise NotImplementedError
+
+
+class Atom(ABC):
+    @abc.abstractproperty
+    def atom_type(self):
+        raise NotImplementedError
+        return None
+
+    @abc.abstractproperty
+    def charge(self):
+        raise NotImplementedError
+        return self.atom_type.charge
+
+    @abc.abstractproperty
+    def mass(self):
+        raise NotImplementedError
+        return self.atom_type.mass
+
+
+class AtomType(ABC):
+    @abc.abstractproperty
+    def name(self):
+        raise NotImplementedError
+        return ''
+
+    @abc.abstractproperty
+    def charge(self):
+        raise NotImplementedError
+        return 0
+
+    @abc.abstractproperty
+    def mass(self):
+        raise NotImplementedError
+        return 0
+
+    def __str__(self):
+        return self.name
